9f09227b89b51061f836268907576c1b646cf007
Alireza Balouch <gswape@gmail.com>
2025-05-06 13:01:37 +0200

    chore: update dependencies and improve component styles
    - Updated devDependencies in package.json:
      - @typescript-eslint/parser to ^8.32.0
      - astro to ^5.7.10
      - prettier to ^3.5.3
    - Updated dependencies in package.json:
      - @tailwindcss/vite to ^4.1.5
      - tailwindcss to ^4.1.5
    - Refactored Footer, Menu, and TopBar components to include a reference to the global styles.css.
    - Improved code readability in Footer component by simplifying conditional logic.
    - Added charset meta tag in normal.astro layout for better character encoding support.
    - Removed redundant Tailwind directives in styles.css and replaced with @import statement.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,10 @@
 import { defineConfig } from 'astro/config'
-
-import tailwind from '@astrojs/tailwind'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://astro.build/config
 
 export default defineConfig({
-  integrations: [
-    tailwind({
-      applyBaseStyles: false,
-    }),
-  ],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 })
